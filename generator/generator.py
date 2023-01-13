cb7a1c783e36582d2ea9732c03342a83295a95fd
test-pps-admin <test.pps.github@gmail.com>
2023-01-14 03:08:27 +0900

    Update: path 'generator/generator.py' - Updated by 'thak00'

--- generator/generator.py
+++ generator/generator.py
@@ -8,8 +8,11 @@ from random import randint as rnd
 hashValue = hashlib.sha256("|".join(sys.argv[1:]).encode()).hexdigest()
 random.seed(hashValue)
 
-l = int(sys.argv[1])
-r = int(sys.argv[2])
+n = int(sys.argv[1])
+l = int(sys.argv[2])
+r = int(sys.argv[3])
 
-a, b, c = [rnd(l, r) for _ in range(3)]
-print(a, b, c)
+a = [rnd(l, r) for _ in range(n)]
+print(n)
+for i in a:
+  print(i)
